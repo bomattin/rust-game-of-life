32994bba13c769256f37743fb2ff051dda529744
bomattin <bomattin@butler.edu>
2015-05-17 13:53:18 -0400

    We have a grid, call Jeff Bridges.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::io;
+
+const WIDTH: usize = 32;
+const HEIGHT: usize = 16;
+
+fn display_grid(grid: &[[bool;WIDTH]]) {
+    for &row in grid.iter() {
+        for &cell in row.iter() {
+            if(cell) {
+                print!(" O");
+            } else {
+                print!(" .");
+            }
+
+        }
+        println!("");
+    }
+}
+
 fn main() {
-    println!("Hello world!")
+
+    let mut grid = [[false;WIDTH];HEIGHT];
+    grid[3][9] = true;
+    grid[4][9] = true;
+    grid[5][9] = true;
+    grid[6][9] = true;
+
+    display_grid(&grid);
 }
